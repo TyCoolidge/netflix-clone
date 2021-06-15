dc7805f7006ba6ca102629aa30617db56896dcb7
TyC <62319016+TyCoolidge@users.noreply.github.com>
2021-06-15 15:06:03 -0700

    hour 2 completed

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -20,4 +20,34 @@ export const Column = styled.div`
 
 export const Row = styled.div`
   display: grid;
-  grid-template-colunns
+  grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
+  grid-gap: 15px;
+
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  }
+`;
+
+export const Link = styled.a`
+  color: #757575;
+  margin-bottom: 20px;
+  font-size: 14px;
+  text-decoration: none;
+`;
+
+export const Title = styled.p`
+  font-size: 16px;
+  color: #757575;
+  margin-bottom: 40px;
+`;
+
+export const Text = styled.p`
+  font-size: 13px;
+  color: #757575;
+  margin-bottom: 40px;
+`;
+
+export const Break = styled.p`
+  flex-basis: 100%;
+  height: 0;
+`;
